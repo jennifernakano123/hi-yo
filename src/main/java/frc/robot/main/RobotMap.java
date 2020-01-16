90744935eb357c3fdb8f5ea46d7943ba2b1657be
jennifernakano123 <59908819+jennifernakano123@users.noreply.github.com>
2020-01-15 20:38:26 -1000

    Create RobotMap.java

---
+++ src/main/java/frc/robot/main/RobotMap.java
@@ -0,0 +1,24 @@
+package frc.robot.main;
+
+/**
+ * Add your docs here.
+ */
+public class RobotMap {
+    public static int 
+        m_frontRight = 6,
+        m_frontLeft = 4,
+        m_rearRight = 7  ,
+        m_rearLeft = 5;
+    public static int
+        m_arm_rotator = 6;
+    public static int
+        m_shooter = 8;
+    public static int
+        hopper_adjust = 9,
+        hopper_feeder = 10;
+    public static int
+        hang_lift = 14,
+        hang_adjust = 15;
+    public static int
+        joystick = 0;
+}
